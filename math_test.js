08f49703249f4ba58503f492bd5cf2e214cbed68
Pedro Ramos <ramuspedro@gmail.com>
2022-05-19 20:38:02 -0300

    criando novo teste

--- math_test.js
+++ math_test.js
@@ -2,7 +2,10 @@ const assert = require('assert');
 const multiply = require("./math.js");
 
 describe('Teste multiplicação', () => {
-	it('10 * 10 = 100', () => {
+	it('10 vezes 10 é igual a 100', () => {
 		assert.equal(100, multiply(10,10), "O resultado eh 100");
+	});
+	it('8 vezes 8 é diferente de 100', () => {
+		assert.notEqual(100, multiply(8,8), "O resultado não eh 100");
 	});
 });
