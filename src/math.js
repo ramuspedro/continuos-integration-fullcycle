13a2b72e718cdc67213e30141c968535550ac4c8
Pedro Ramos <ramuspedro@gmail.com>
2022-05-31 10:10:46 -0300

    add: sub2

--- src/math.js
+++ src/math.js
@@ -6,6 +6,10 @@ const sub = (x, y) => {
 	return x - y;
 }
 
+const sub2 = (x, y) => {
+	return x - y;
+}
+
 const add = (x, y) => {
 	return x + y;
 }
@@ -14,4 +18,4 @@ const add2 = (x, y) => {
 	return x + y;
 }
 
-module.exports = { multiply, sub, add, add2 };
+module.exports = { multiply, sub, sub2, add, add2 };
